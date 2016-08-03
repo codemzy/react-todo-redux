82894e1c23006062e6c5cb5073e5ccd731ec2943
Emma J <codemzy@gmail.com>
2016-08-03 10:17:48 +0000

    Refactored TodoApp.js

--- app/components/TodoApp.js
+++ app/components/TodoApp.js
@@ -1,40 +1,13 @@
 var React = require('react');
-var moment = require('moment');
 
 // components
 import TodoList from './TodoList.js';
 import TodoForm from './TodoForm.js';
 import TodoSearch from './TodoSearch.js';
 
-// api
-var TodoAPI = require('../api/TodoAPI.js');
-
 class TodoApp extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            showCompleted: false,
-            searchText: '',
-            todos: TodoAPI.getTodos()
-        };
-    }
-    
-    componentDidUpdate() {
-        TodoAPI.setTodos(this.state.todos);
-    }
-    
-    _handleAddTodo(text) {
-        var lastID = 0;
-        if (this.state.todos.length > 0) {
-            lastID = this.state.todos[this.state.todos.length-1].id;
-        }
-        var newID = lastID + 1;
-        this.setState({
-            todos: [
-            ...this.state.todos,
-            { id: newID, text: text, completed: false, createdAt: moment().unix(), completedAt: false }
-            ]
-        });
     }
     
     render() {
@@ -44,7 +17,7 @@ class TodoApp extends React.Component {
                 <div className="todowrap">
                     <TodoSearch />
                     <TodoList />
-                    <TodoForm onAdd={this._handleAddTodo.bind(this)} />
+                    <TodoForm />
                 </div>
             </div>
         );
